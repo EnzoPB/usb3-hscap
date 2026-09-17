99ffca3552d96ea1719bef9861d30dccadb1755a
Enzo PB <git@enzopb.me>
2026-09-17 10:44:52 +0300

    feat: jpeg compression & simd conversion

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #include <sys/types.h>
 #include <string.h>
 #include <stdbool.h>
+#include <turbojpeg.h>
+#include <Simd/SimdLib.h>
 
 #define NUM_BUFFERS 100
 #define DISPLAY_FRAMERATE 10
@@ -46,8 +48,9 @@ int main(int argc, char **argv)
     signal(SIGPIPE, intHandler);
     
     // Give some time for the system/camera to settle if needed
-    g_usleep(5e6);
+    g_usleep(3e6);
     
+    tjhandle tj_compressor = NULL;
     ArvCamera *camera;
     GError *error = NULL;
 
@@ -60,6 +63,20 @@ int main(int argc, char **argv)
     }
 
     fprintf(stderr, "Connected to %s\n", arv_camera_get_model_name(camera, NULL));
+
+    arv_camera_set_frame_rate(camera, CAPTURE_FRAMERATE, &error);
+    if (error != NULL) {
+        fprintf(stderr, "Error: failed to set framerate: %s\n", error->message);
+        g_clear_error(&error);
+    }
+
+    arv_camera_set_pixel_format(camera, ARV_PIXEL_FORMAT_BAYER_BG_8, &error);
+    if (error != NULL) {
+        fprintf(stderr, "Error: failed to set pixel format: %s\n", error->message);
+        g_clear_error(&error);
+    }
+
+    fprintf(stderr, "Pixel format: %s %d\n", arv_camera_get_pixel_format_as_string(camera, NULL), arv_camera_get_pixel_format(camera, NULL));
 
     arv_camera_set_acquisition_mode(camera, ARV_ACQUISITION_MODE_CONTINUOUS, &error);
     if (error != NULL) {
@@ -101,6 +118,11 @@ int main(int argc, char **argv)
         if (stat("images", &st) == -1) {
             mkdir("images", 0700);
         }
+        tj_compressor = tjInitCompress();
+        if (tj_compressor == NULL) {
+            fprintf(stderr, "Error initializing turbojpeg compressor\n");
+            goto cleanup_stream;
+        }
     }
 
     guint64 last_display_ts = 0;
@@ -120,6 +142,7 @@ int main(int argc, char **argv)
         }
 
         guint64 ts = arv_buffer_get_timestamp(buffer);
+
         if (i % CAPTURE_FRAMERATE == 0) {
             if (last_fps_ts != 0) {
                 double delta_s = (ts - last_fps_ts) / 1e9;
@@ -149,16 +172,56 @@ int main(int argc, char **argv)
 
         if (save_frames) {
             clock_gettime(CLOCK_MONOTONIC, &t1);
-            char image_filename[64];
-            snprintf(image_filename, sizeof(image_filename), "images/%lu.raw", i);
+            
+            size_t width = arv_buffer_get_image_width(buffer);
+            size_t height = arv_buffer_get_image_height(buffer);
 
-            FILE *f = fopen(image_filename, "wb");
-            if (f) {
-                fwrite(image, 1, image_size, f);
-                fclose(f);
+            // convert to bgr, jpeg doesn't support bayer pixel format
+            // use SIMD to be fast (ka-chow)
+            unsigned char *bgr_buf = SimdAllocate(width * height * 3, 2);
+
+            if (bgr_buf != NULL) {
+                SimdBayerToBgr(
+                    (const unsigned char*)image,
+                    width, height,
+                    width,
+                    SimdPixelFormatBayerBggr,
+                    bgr_buf, width*3
+                );
+                
+                unsigned char *jpeg_buf = NULL;
+                unsigned long jpeg_size = 0;
+
+                if (tjCompress2(
+                    tj_compressor,
+                    bgr_buf,
+                    width, 0, height,
+                    TJPF_BGR,
+                    &jpeg_buf,
+                    &jpeg_size,
+                    TJSAMP_GRAY,
+                    90,
+                    TJFLAG_FASTDCT
+                ) == 0) {
+                    char image_filename[64];
+                    snprintf(image_filename, sizeof(image_filename), "images/%010lu.jpg", i);
+
+                    FILE *f = fopen(image_filename, "wb");
+                    if (f) {
+                        fwrite(jpeg_buf, 1, jpeg_size, f);
+                        fclose(f);
+                    } else {
+                        fprintf(stderr, "Error: could not open %s\n", image_filename);
+                    }
+                    tjFree(jpeg_buf);
+                } else {
+                    fprintf(stderr, "Error: turbojpeg compression failure: %s\n", tjGetErrorStr());
+                }
             } else {
-                fprintf(stderr, "Error: could not open %s\n", image_filename);
+                fprintf(stderr, "Error: could not allocate BGR buffer\n");
             }
+            SimdFree(bgr_buf);
+            
             clock_gettime(CLOCK_MONOTONIC, &t2);
         }
 
@@ -169,17 +232,17 @@ int main(int argc, char **argv)
     arv_camera_stop_acquisition(camera, &error);
     if (error != NULL) {
         fprintf(stderr, "Error stopping acquisition: %s\n", error->message);
-        g_error_free(error);
-        error = NULL;
+        g_clear_error(&error);
     }
 
 cleanup_stream:
+    if (tj_compressor != NULL) {
+        tjDestroy(tj_compressor);
+    }
     g_clear_object(&stream);
 cleanup_camera:
     g_clear_object(&camera);
-    if (error != NULL) {
-        g_error_free(error);
-    }
+    g_clear_error(&error);
 
     fprintf(stderr, "exit\n");
     return EXIT_SUCCESS;
